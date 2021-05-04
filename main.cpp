95a7fec95af18ce2c772b788f5dbcc9bb7b10faf
nitsuga09 <82417727+nitsuga09@users.noreply.github.com>
2021-05-03 22:56:22 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,22 @@
 #include <Arduino.h>
 #include <boton.h>
 
-boton::boton(byte pin)
-{
-    _pin = pin;
-    pinMode(_pin, INPUT);
-    debounceTimeMs = 100;
-    state = Idle;
+boton miBoton1(18);
+boton miBoton2(19);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Iniciando programa");
 }
 
-void boton::Refresh() {
-    switch(state){
-        case Idle:
-            pressType = NoPress;
-            if(digitalRead(_pin)){
-                state = DevouncingFirstPress;
-                startDebouncingMillis = millis();
-            };
-            break;
-        case DevouncingFirstPress:
-            if(millis() - startDebouncingMillis >= debounceTimeMs){
-                if(digitalRead(_pin)){
-                    state = WaitingStopPress;   //Si todavia esta pretado el boton (digital read) se va a poner en el estado WaitingStopPress si no esta presionado se pone en estado Idle
-                };
-            };
-        break;
-        case WaitingStopPress:
-            if(!digitalRead(_pin)){
-                state = Idle;
-                pressType = Press;
-            };
-            break;
-
-        default:
-            break;
-    }
+void loop() {
+  miBoton1.Refresh();
+  miBoton2.Refresh();
+  if(miBoton1.GetPressType() == miBoton1.Press){
+    Serial.print("\n Boton 1 presionado");
+  }
+  if(miBoton2.GetPressType() == miBoton2.Press){
+    Serial.print("\n Boton 2 presionado");
+  }
+  // put your main code here, to run repeatedly:
 }
-
-boton::PressTypes boton::GetPressType() {
-    return pressType;
-};
